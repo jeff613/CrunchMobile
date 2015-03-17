f0411aab4e9a4b4123214705a2ba586eb40ca3d6
Anoop tomar <anooptomar@Anoops-macbook.local>
2015-03-16 23:31:21 -0700

    sample call to api

--- CrunchMobile/CrunchClient.swift
+++ CrunchMobile/CrunchClient.swift
@@ -16,7 +16,12 @@ class CrunchClient{
     
     class func getOrgList(skip: Int, count: Int){
         CrunchClient.SharedInstance.GET(sharedVariables.apiBaseUrl+"info?skip=\(skip)&count=\(count)", parameters: nil, success: { (operation: AFHTTPRequestOperation!, response: AnyObject!) -> Void in
-            println(response)
+            var results = response as NSArray
+            for index in 0..<results.count{
+                println("------------------------------------------")
+                println(results[index])
+                println("------------------------------------------")
+            }
             }, failure: {(operation: AFHTTPRequestOperation!, error:NSError!) -> Void in
             
             })
